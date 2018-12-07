1e8271bb62a55bde88d1c202f57da7ca1cff569a
Joel Foster <joelsfoster@gmail.com>
2018-12-06 22:07:13 -0500

    Filters for location and identifies desiredClass to be booked
    Still need to actually finish the RSVP. And need to make the cron run
    at the right time for each class.

--- classes.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  {
-    id: 0,
-    name: 'Functional Training',
-    time: '6:00 AM - 7:00 AM',
-    address: '41st & 3rd',
-    room: 'UXF Zone'
-  }
-};
--- nysc.js
+++ nysc.js
@@ -3,16 +3,15 @@ const moment = require('moment');
 // const cron = require('node-cron');
 
 const nyscCredentials = require('./nyscCredentials.js');
-// const classes = require('./classes.js');
 
 
 // Start the cron when this file is run
-// cron.schedule('* * * * *', () => {
+// cron.schedule('* * * * *', () => { // TODO make this cron run immediately when a class' RSVPs are open
 //   console.log('running every minute');
 //   nysc();
 // });
 
-const nysc = async () => {
+const nysc = async (desiredClass) => {
   try {
 
     // Initiate Puppeteer
@@ -33,11 +32,11 @@ const nysc = async () => {
     await page.type('#password', nyscCredentials.password);
     await page.click('#_submit');
 
-    // Navigate to class page
+    // Navigate to class page and filter your gym
     const targetDay = moment().add(7, 'days').format("MM/DD");
-    await page.goto('https://www.newyorksportsclubs.com/classes' + `?day=${targetDay}`);
+    await page.goto(`https://www.newyorksportsclubs.com/classes?day=${targetDay}&club=${desiredClass.location}`);
 
-    // Load all that there is to load (takes care of pagination so we grab ALL events there are to see!)
+    // Load all that there is to load (takes care of any pagination)
     const loadMore = async () => {
       try {
         await page.click('#load-more');
@@ -51,14 +50,16 @@ const nysc = async () => {
     // Get all the event rows then loop through each of them
     const events = await page.$$('#events-list > div.row');
     for (const e of events) {
+      const tryWrapper = async (func) => { try { return func } catch (err) { return null } };
+
       const event = {
-        name:         await e.$eval('a.bigger', eventName => eventName.innerText),
-        duration:     await e.$$eval('li.table-list-item', eventDuration => eventDuration[2].innerText), // there are 3 lis, the third has what we want
-        instructor:   await e.$eval('a.link', eventInstructor => eventInstructor.innerText),
-        time:         await e.$eval('span.big', eventTime => eventTime.innerText), // TODO separate start time and end time
-        room:         await e.$eval('span.room', eventRoom => eventRoom.innerText.substring(2)), // removes unnecessary "@ " prefix in the string
-        address:      await e.$eval('span.address', eventAddress => eventAddress.innerText),
-        link:         await e.$eval('a.reserve', eventLink => eventLink.href) // TODO FIX THIS WHEN BUTTON IS DISABLED
+        name:         await tryWrapper(e.$eval('a.bigger', eventName => eventName.innerText)),
+        duration:     await tryWrapper(e.$$eval('li.table-list-item', eventDuration => eventDuration[2].innerText)), // there are 3 lis, the third has what we want
+        instructor:   await tryWrapper(e.$eval('a.link', eventInstructor => eventInstructor.innerText)),
+        time:         await tryWrapper(e.$eval('span.big', eventTime => eventTime.innerText.substring(0,8).trim())), // Parses out the start time
+        room:         await tryWrapper(e.$eval('span.room', eventRoom => eventRoom.innerText.substring(2))), // removes unnecessary "@ " prefix in the string
+        address:      await tryWrapper(e.$eval('span.address', eventAddress => eventAddress.innerText)),
+        link:         await tryWrapper(e.$eval('a.reserve', eventLink => eventLink.href))
       }
 
       console.log('\n');
@@ -66,16 +67,23 @@ const nysc = async () => {
       console.log(targetDay, '|', event.time);
       console.log(event.room, '@', event.address);
       console.log(event.link);
+
+      // Book the class if it's available
+      if (event.name == desiredClass.name && event.time == desiredClass.time) {
+        page.goto(event.link);
+        // TODO next, click "reserve"
+      }
     }
-
-    // console.log(events.length);
-    // await page.goto('https://www.newyorksportsclubs.com/classes/35197705/reserve');
-
-
 
   } catch (err) {
     console.log('*** ERROR ON OUR END ***:', err);
   }
 }
 
-nysc();
+// Run the function with the specified params
+const _desiredClass = { // TODO Let the user specify this
+  name: "Cycling",
+  time: "9:00 AM",
+  location: "astoria" // CASE SENSITIVE!!! words are separated by dashes
+}
+nysc(_desiredClass);
